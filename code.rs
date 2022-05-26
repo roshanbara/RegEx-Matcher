388fea2452a2f0dca1c2ba5cd7ee91d19d067137
Roshan Prashant Bara <79328692+roshanbara@users.noreply.github.com>
2022-05-26 12:25:37 +0530

    Add files via upload

---
+++ code.rs
@@ -0,0 +1,60 @@
+use std::process;
+use std::io;
+
+pub fn nfa(state: i32, strlang: String) {
+    if strlang.len() == 0 && (state != 3 && state != 4 && state != 6) {
+        println!("Last State {}", state);
+        println!("String REJECTED!");
+        process::exit(1);
+    }
+    if strlang.len() == 0 && (state == 3 || state == 4 || state == 6) {
+        println!("Last State {}", state);
+        println!("String ACCEPTED!");
+        process::exit(1);
+    }
+    let alpha = strlang.chars().nth(0);
+    let substr = String::from(strlang.get(1..).unwrap());
+    if state == 0 && alpha==Some('a') {
+        println!("State {}", state);
+        nfa(4, substr);
+    } else if state == 4 && alpha == Some('b') {
+        println!("State {}", state);
+        nfa(5, substr);
+    } else if state == 5 && alpha == Some('c') {
+        println!("State {}", state);
+        nfa(6, substr);
+    } else if state == 6 && alpha == Some('b') {
+        println!("State {}", state);
+        nfa(5, substr);
+    } else if state == 0 && alpha == Some('b') {
+        println!("State {}", state);
+        nfa(1, substr);
+    } else if state == 1 && alpha == Some('b') {
+        println!("State {}", state);
+        nfa(3, substr);
+    } else if state == 1 && alpha == Some('a') {
+        println!("State {}", state);
+        nfa(2, substr);
+    } else if state == 2 && alpha == Some('a') {
+        println!("State {}", state);
+        nfa(2, substr);
+    } else if state == 2 && alpha == Some('b') {
+        println!("State {}", state);
+        nfa(3, substr);
+    } else {
+        println!("State {}", state);
+        println!("String REJECTED!");
+    }
+}
+
+fn main() {
+    println!("RegEx: ba*b + a(bc)*");
+    println!("Enter a String:");
+    let mut stra = String::new();
+	io::stdin()
+		.read_line(&mut stra)
+		.expect("failed to read input.");
+
+    let a= String::from(stra.trim());
+    nfa(0, a);
+}
